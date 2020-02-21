4b89354b1a782012e90a05aece2a3a8bbd8cc866
Maria Ronauli <mronauli@gmail.com>
2020-02-21 09:29:01 -0700

    add model test

---
+++ spec/features/dishes/visitor_can_see_a_dish_spec.rb
@@ -0,0 +1,29 @@
+require "rails_helper"
+
+RSpec.describe "on a dish show page" do
+  context "as a visitor" do
+    xit "can see a dish, its chef and list of ingredients" do
+      chef_1 = Chef.create(name: "Boyardee")
+      dish_1 = Dish.create(name: "Pizza", description: "Very nice", chef: chef_1)
+      dish_2 = Dish.create(name: "Spaghetti", description: "Yum", chef: chef_1)
+      ingredient_1 = Ingredient.create(name: "Cheese", calories: 50, dishes: [dish_1])
+      ingredient_2 = Ingredient.create(name: "Bread", calories: 100, dishes: [dish_1, dish_2])
+      ingredient_3 = Ingredient.create(name: "Pepperoni", calories: 30, dishes: [dish_1])
+      ingredient_4 = Ingredient.create(name: "Cilantro", calories: 30, dishes: [dish_2])
+
+      visit "/dish/#{dish_1.id}"
+      expect(page).to have_content(dish_1.name)
+      expect(page).to have_content(chef_1.name)
+      expect(page).to have_content(ingredient_1.name)
+      expect(page).to have_content(ingredient_2.name)
+      expect(page).to have_content(ingredient_3.name)
+      expect(page).to_not have_content(ingredient_4.name)
+    end
+  end
+end
+
+# Story 1 of 3
+# As a visitor
+# When I visit a dish's show page
+# I see the name of the chef that made that dish
+# And I see a list of ingredients for that dish
