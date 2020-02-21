343e6c1174d2fdf8f47ea3e3d836a6461b16ade7
Maria Ronauli <mronauli@gmail.com>
2020-02-21 09:36:34 -0700

    add route

--- spec/features/dishes/visitor_can_see_a_dish_spec.rb
+++ spec/features/dishes/visitor_can_see_a_dish_spec.rb
@@ -2,7 +2,7 @@ require "rails_helper"
 
 RSpec.describe "on a dish show page" do
   context "as a visitor" do
-    xit "can see a dish, its chef and list of ingredients" do
+    it "can see a dish, its chef and list of ingredients" do
       chef_1 = Chef.create(name: "Boyardee")
       dish_1 = Dish.create(name: "Pizza", description: "Very nice", chef: chef_1)
       dish_2 = Dish.create(name: "Spaghetti", description: "Yum", chef: chef_1)
@@ -11,7 +11,7 @@ RSpec.describe "on a dish show page" do
       ingredient_3 = Ingredient.create(name: "Pepperoni", calories: 30, dishes: [dish_1])
       ingredient_4 = Ingredient.create(name: "Cilantro", calories: 30, dishes: [dish_2])
 
-      visit "/dish/#{dish_1.id}"
+      visit "/dishes/#{dish_1.id}"
       expect(page).to have_content(dish_1.name)
       expect(page).to have_content(chef_1.name)
       expect(page).to have_content(ingredient_1.name)
