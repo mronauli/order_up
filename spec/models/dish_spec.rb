aac4221d234aef44a1748184120fc0b2f72f242f
Maria Ronauli <mronauli@gmail.com>
2020-02-21 09:44:16 -0700

    add method for total cals

--- spec/models/dish_spec.rb
+++ spec/models/dish_spec.rb
@@ -10,4 +10,15 @@ RSpec.describe Dish, type: :model do
     it {should have_many :dish_ingredients}
     it {should have_many(:ingredients).through(:dish_ingredients)}
   end
+
+  describe "#total_cals" do
+    it "can calculate total calories of a dish" do
+      chef_1 = Chef.create(name: "Boyardee")
+      dish_1 = Dish.create(name: "Pizza", description: "Very nice", chef: chef_1)
+      ingredient_1 = Ingredient.create(name: "Cheese", calories: 50, dishes: [dish_1])
+      ingredient_2 = Ingredient.create(name: "Bread", calories: 100, dishes: [dish_1])
+      ingredient_3 = Ingredient.create(name: "Pepperoni", calories: 30, dishes: [dish_1])
+      expect(dish_1.total_cals).to eq(180)
+    end
+  end
 end
